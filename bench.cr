266ae9a346c2405ed6751a5eb353fc785a25b1c9
Konovod <konovod@gmail.com>
2017-06-15 21:24:49 +0000

    testhash working

--- bench.cr
+++ bench.cr
@@ -3,8 +3,8 @@ require "./src/robinhash"
 require "benchmark"
 
 def try(hsh, n)
-  rnd = Random.new(1)
-  sum = 0
+  rnd = Random.new(2)
+  sum = 0i64
   # addition
   (1..n).each do |i|
     hsh[rnd.rand(n)] = i*i
@@ -29,24 +29,52 @@ def try(hsh, n)
   sum
 end
 
-p try(Hash(Int32, Int32).new(nil), 100)
-p try(TestHash::MyHash(Int32, Int32).new(nil), 100)
-p try(TestHash::RobinHash(Int32, Int32).new(nil), 100)
+def try2(hsh1, hsh2, iter, n)
+  sum1 = sum2 = 0
+  iter.times do |i|
+    rnd = Random.new(i)
+    # addition
+    rnd.rand(n/2).times do |i|
+      v = rnd.rand(n)
+      hsh1[v] = i*i
+      hsh2[v] = i*i
+    end
+    # deleting
+    rnd.rand(n/2).times do |i|
+      v = rnd.rand(n)
+      hsh1.delete(v) { }
+      hsh2.delete(v) { }
+    end
+    # lookup after changes
+    rnd.rand(n/2).times do |i|
+      v = rnd.rand(n)
+      sum1 += hsh1[v]? || 0
+      sum2 += hsh2[v]? || 0
+      if sum1 != sum2
+        pp v, hsh1[v]?, hsh2[v]?
+        raise ""
+      end
+    end
+  end
+end
 
+try2(Hash(Int32, Int32).new(nil), TestHash::MyHash(Int32, Int32).new(nil), 30, 10000)
+# p try2(TestHash::RobinHash(Int32, Int32).new(nil), 100, 100)
+#
 x = 0
 Benchmark.ips do |bench|
   bench.report("default") { x += try(Hash(Int32, Int32).new(nil), 10) }
   bench.report("my") { x += try(TestHash::MyHash(Int32, Int32).new(nil), 10) }
-  bench.report("robin") { x += try(TestHash::RobinHash(Int32, Int32).new(nil), 10) }
+  # bench.report("robin") { x += try(TestHash::RobinHash(Int32, Int32).new(nil), 10) }
 end
 Benchmark.ips do |bench|
   bench.report("default avg") { x += try(Hash(Int32, Int32).new(nil), 125) }
   bench.report("my avg") { x += try(TestHash::MyHash(Int32, Int32).new(nil), 125) }
-  bench.report("robin avg") { x += try(TestHash::RobinHash(Int32, Int32).new(nil), 125) }
+  # bench.report("robin avg") { x += try(TestHash::RobinHash(Int32, Int32).new(nil), 125) }
 end
 Benchmark.ips do |bench|
   bench.report("default big") { x += try(Hash(Int32, Int32).new(nil), 10000) }
   bench.report("my big") { x += try(TestHash::MyHash(Int32, Int32).new(nil), 10000) }
-  bench.report("robin big") { x += try(TestHash::RobinHash(Int32, Int32).new(nil), 10000) }
+  # bench.report("robin big") { x += try(TestHash::RobinHash(Int32, Int32).new(nil), 10000) }
 end
 p x
--- src/testhash.cr
+++ src/testhash.cr
@@ -8,45 +8,78 @@ module TestHash
 
   class MyHash(K, V)
     MAX_LOAD_FACTOR = 75
-    MIN_LOAD_FACTOR = 25
+    MIN_LOAD_FACTOR =  5
+
+    # @check = Hash(K, V).new
 
     def initialize(*args)
       @used = 0
+      @tombs = 0
       @allocated = 3
       @data = Slice((Status | {K, V})).new(1 << @allocated) { Status::Empty.as((Status | {K, V})) }
     end
 
+    # def sanity
+    #   @check.values.sum == @data.sum { |x| x.is_a?(Status) ? 0 : x[1] }
+    # end
+
     def []=(key, value)
-      @used += 1
-      rehash(@allocated + 1) if @used > MAX_LOAD_FACTOR*(1 << @allocated)/100
       index = lookup(key, false)
+      @used += 1 if @data[index].is_a? Status
+
+      # if index > 2 && index < @data.size - 5
+      #   copy = @data[index - 2, 5].map &.inspect
+      #   xxx = @check[key]?
+      #   yyy = self[key]?
+      #   oldsum = @check.values.sum
+      # end
+
       @data[index] = {key, value}
+
+      # @check[key] = value
+      # unless sanity
+      #   pp xxx, yyy, oldsum, @check.values.sum
+      #   pp @data.sum { |x| x.is_a?(Status) ? 0 : x[1] }
+      #   pp copy, @data[index - 2, 5], index, key, value
+      #   raise ""
+      # end
+
+      rehash(@allocated + 1) if @used + @tombs > MAX_LOAD_FACTOR*(1 << @allocated)/100
     end
 
     def []?(key)
       index = lookup(key, true)
       case x = @data[index]
       when Status
+        # raise "" if @check[key]?
         return nil
       else
+        # raise "" if @check[key]? != x[1]
         return x[1]
       end
     end
 
     def delete(key, &block)
-      @used -= 1
       index = lookup(key, true)
-      unless @data[index].is_a?(Status)
+      # @check.delete(key) { yield }
+      if @data[index].is_a?(Status)
+        yield
+      else
+        @used -= 1
+        @tombs += 1
         @data[index] = Status::Tombstone
         rehash(@allocated - 1) if @allocated > 3 && @used < MIN_LOAD_FACTOR*(1 << @allocated)/100
       end
     end
 
     private def rehash(new_size)
+      # p "rehash #{new_size}"
+      # @check.clear
       old = @data
       @allocated = new_size
       @data = Slice((Status | {K, V})).new(1 << @allocated) { Status::Empty.as((Status | {K, V})) }
       @used = 0
+      @tombs = 0
       old.each do |x|
         self[x[0]] = x[1] unless x.is_a?(Status)
       end
@@ -60,12 +93,13 @@ module TestHash
       loop do
         case v = @data[index]
         when Status
-          return index if (!search) || v == Status::Empty
+          return index if v == Status::Empty
         else
           return index if v[0] == key
         end
         index = (index + delta*delta) & mask
         delta += 1
+        # raise "#{inspect}" if delta > mask
       end
     end
   end
